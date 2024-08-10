1d1f418cc50107d2a4385aea27eb73ba51be3397
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-08-11 02:50:07 +0600

    setup the project

--- E-commerce product page/src/App.jsx
+++ E-commerce product page/src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-orange-500">Hello, Welcome to this page!</h1>
+      <h1 className="text-orange-500 text-3xl">Hello, Welcome to this page!</h1>
     </>
   );
 }
