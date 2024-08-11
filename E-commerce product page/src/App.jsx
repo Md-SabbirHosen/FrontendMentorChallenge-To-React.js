8dbbc9b3f22680c60f8757cc4e22e78c6eb5d992
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-08-11 17:01:22 +0600

    updated

--- E-commerce product page/src/App.jsx
+++ E-commerce product page/src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1 className="text-orange-500 text-3xl">Hello, Welcome to this page!</h1>
+      <div className="container">
+        <h1 className="text-orange-500  font-kumbh">
+          Hello, Welcome to this page!
+        </h1>
+      </div>
     </>
   );
 }
--- E-commerce product page/tailwind.config.js
+++ E-commerce product page/tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
+import { plugin } from "postcss";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: ["Kumbh Sans", "system-ui"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities }) {
+      addBase({});
+      addComponents({
+        ".container": {
+          "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]":
+            {},
+        },
+      });
+      addUtilities({
+        ".tap-highlight-color": {
+          "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)",
+        },
+      });
+    }),
+  ],
 };
