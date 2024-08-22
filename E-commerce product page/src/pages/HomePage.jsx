75114b11fe17d10e29b8eedb7c1b072ded2025fd
Jawwad926 <mdsabbirhosen926@gmail.com>
2024-08-22 18:25:43 +0600

    update homepage

--- E-commerce product page/src/pages/HomePage.jsx
+++ E-commerce product page/src/pages/HomePage.jsx
@@ -38,16 +38,29 @@ const HomePage = () => {
       </div>
 
       <div className="flex flex-col p-6 md:basis-1/2">
-        <p>Sneaker Company</p>
-        <h1>
-          Fall Limited Edition <br /> Sneaker
+        <p className="font-bold uppercase tracking-widest text-sm mb-6">
+          Sneaker Company
+        </p>
+        <h1 className="text-3xl text-black font-bold  md:text-4xl">
+          Fall Limited Edition <br /> Sneakers
         </h1>
         <p className="">
           These low-profile sneakers are your perfect casual wear <br />
           companion. Featuring a durable rubber outer sole, they'll <br />
           withstand everything the weather can offer.
         </p>
-        <div></div>
+        <div className="flex md:flex-col">
+          <div className="flex">
+            <p>$125.00</p>
+            <p>50%</p>
+          </div>
+          <p>$250.00</p>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <button>-</button>
+          <p>0</p>
+          <button>+</button>
+        </div>
       </div>
     </>
   );
